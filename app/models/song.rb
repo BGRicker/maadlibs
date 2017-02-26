e2d4cc613361f3600a0ef9a7239f4dfb73a5250a
Ben Ricker <BGRicker@gmail.com>
2017-02-25 23:58:17 -0500

    method for swapping words

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,11 @@
 class Song < ApplicationRecord
+
+	def swapper(string, swear_set, user_input)
+		string.split.each do |word|
+			if swear_set.include?(word)
+				word.gsub!(word, user_input.sample)
+			end
+		end
+	end
+
 end
